286bd0507ce1bf3350e448ee9e571b5f2c89bcf6
Nandeesh Subbegowda <subbegowda@gmail.com>
2023-05-15 23:33:38 +0100

    added new file app.py

---
+++ tut1/app.py
@@ -0,0 +1,10 @@
+
+print("Printing before calling printMe function")
+
+def printMe():
+    print("Printing Myself!!")
+
+print("Printing after calling printMe function")
+
+if __name__ == '__main__':
+    printMe()
